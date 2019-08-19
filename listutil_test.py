7a03ce3d5df09b26f295ba484eb581afb161ffba
KornSiwat <ponpued@gmail.com>
2019-08-19 21:23:03 +0700

    renamed test cases

--- listutil_test.py
+++ listutil_test.py
@@ -8,27 +8,29 @@ class UniqueTest(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual([], unique([]))
 
-    def test_one_element(self):
+    def test_one_item_with_same_type(self):
         self.assertEqual(["isp"], unique(["isp"]))
         self.assertEqual([[]], unique([[]]))
 
-    def test_one_element_many_times(self):
+    def test_one_item_many_times_with_same_type(self):
         self.assertEqual(["isp"], unique(["isp", "isp", "isp", "isp", "isp"]))
         self.assertEqual([[1]], unique([[1], [1], [1]]))
         self.assertEqual([2**16, 2**9], unique([2**16, 2**9, 2**9, 2**16]))
 
-    def test_two_element(self):
+    def test_many_items_no_duplication(self):
         self.assertEqual(["isp", "ske"], unique(["isp", "ske"]))
         self.assertEqual([[1], [2]], unique([[1], [2]]))
 
-    def test_two_element_many_times(self):
+    def test_many_duplicate_items_with_same_type(self):
         self.assertEqual([1, 2], unique([1, 1, 1, 2, 2, 1, 1, 2, 1, 1]))
+
+    def test_many_duplicate_items_with_different_types(self):
         self.assertEqual([1, True], unique([1, True]))
         self.assertEqual([1, "1"], unique([1, "1"]))
         self.assertEqual([True, "True"], unique([True, "True"]))
-        self.assertEqual([1,[[1]]], unique([1, [[1]]]))
+        self.assertEqual([1, [[1]]], unique([1, [[1]]]))
 
-    def test_wrong_type_input(self):
+    def test_wrong_input_type(self):
         with self.assertRaises(TypeError):
             unique(True)
         with self.assertRaises(TypeError):
@@ -37,3 +39,5 @@ class UniqueTest(unittest.TestCase):
             unique(3.145723929)
         with self.assertRaises(TypeError):
             unique("ISP is challenging")
+        with self.assertRaises(TypeError):
+            unique(None)
