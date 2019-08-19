db06614cef8be36b4d03fbd51d6f54052a5d2de4
KornSiwat <ponpued@gmail.com>
2019-08-19 17:32:54 +0700

    added test cases and implemented unique

--- listutil.py
+++ listutil.py
@@ -10,12 +10,30 @@ def unique(list):
     Examples:
     >>> unique([5])
     [5]
-    >>> unique(["b","a","a","b","b","b","a","a"])
-    ["b","a"]
+    >>> unique(['b','a','a','b','b','b','a','a'])
+    ['b', 'a']
     >>> unique([])
     []
+    >>> unique(['isp', 'isp', 'isp', 'isp', 'isp'])
+    ['isp']
+    >>> unique([1, 1, True, 1])
+    [1, True]
+    >>> unique()
     """
-    pass   # remove this and write the actual code
+    uniqued_list = []
+
+    for input_element in list:
+        for uniqued_list_element in uniqued_list:
+            is_same_type = type(input_element) == type(uniqued_list_element)
+            is_same_value = input_element == uniqued_list_element
+
+            if is_same_type and is_same_value:
+                break
+        else:
+            uniqued_list.append(input_element)
+
+    return uniqued_list
+
 
 if __name__ == "__main__":
     """Run the doctests in all methods."""
