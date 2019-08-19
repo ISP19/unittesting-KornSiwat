8597531530910270b87cd4a3762acdd5f343d4ea
KornSiwat <ponpued@gmail.com>
2019-08-19 21:29:46 +0700

    renamed test cases

--- listutil_test.py
+++ listutil_test.py
@@ -8,23 +8,23 @@ class UniqueTest(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual([], unique([]))
 
-    def test_one_item_with_same_type(self):
+    def test_one_item(self):
         self.assertEqual(["isp"], unique(["isp"]))
         self.assertEqual([[]], unique([[]]))
 
-    def test_one_item_many_times_with_same_type(self):
+    def test_one_item_with_duplications(self):
         self.assertEqual(["isp"], unique(["isp", "isp", "isp", "isp", "isp"]))
         self.assertEqual([[1]], unique([[1], [1], [1]]))
         self.assertEqual([2**16, 2**9], unique([2**16, 2**9, 2**9, 2**16]))
 
-    def test_many_items_no_duplication(self):
+    def test_many_items_without_duplication(self):
         self.assertEqual(["isp", "ske"], unique(["isp", "ske"]))
         self.assertEqual([[1], [2]], unique([[1], [2]]))
 
-    def test_many_duplicate_items_with_same_type(self):
+    def test_many_same_type_items_with_duplications(self):
         self.assertEqual([1, 2], unique([1, 1, 1, 2, 2, 1, 1, 2, 1, 1]))
 
-    def test_many_duplicate_items_with_different_types(self):
+    def test_many_different_type_items_with_duplications(self):
         self.assertEqual([1, True], unique([1, True]))
         self.assertEqual([1, "1"], unique([1, "1"]))
         self.assertEqual([True, "True"], unique([True, "True"]))
