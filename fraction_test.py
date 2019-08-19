7091dbcea0fe172c00f929f65d6392d3c45a7595
KornSiwat <ponpued@gmail.com>
2019-08-19 15:44:33 +0700

    added test cases for add, eq

--- fraction_test.py
+++ fraction_test.py
@@ -22,27 +22,31 @@ class FractionTest(unittest.TestCase):
         self.assertEqual("-4", f.__str__())
         f = Fraction(36, -60)
         self.assertEqual("-3/5", f.__str__())
-        # Constructor should provide default denominator = 1
         f = Fraction(99)
         self.assertEqual("99", f.__str__())
 
-    # TODO Write tests for __init__, __eq__, +, *.
-    # Here is an example, but you must add more test cases.
-    # The test requires that your __eq__ is correct.
     def test_add(self):
         # 3/4 = 2/3 + 1/12
         self.assertEqual(Fraction(3, 4), Fraction(1, 12)+Fraction(2, 3))
+        self.assertEqual(Fraction(1, 2), Fraction(25, 100)+Fraction(1, 4))
+        self.assertEqual(Fraction(-3, 2), Fraction(-2, 1)+Fraction(1, 2))
 
     def test_eq(self):
         f = Fraction(1, 2)
         g = Fraction(-40, -80)
         h = Fraction(10000, 20001)  # not quite 1/2
+        i = Fraction(0, 9)
+        j = Fraction(1, -7)
+        k = Fraction(-1, 7)
+        l = Fraction(-1, -2)
         self.assertTrue(f == g)
         self.assertTrue(f.__eq__(g))  # same thing
         self.assertFalse(f == h)
-        self.assertFalse(f.__eq__(h))
-        # TODO write more tests using other cases.
-        # Consider special values like 0, 1/0, -1/0
+        self.assertFalse(f == h)
+        self.assertFalse(i == j)
+        self.assertFalse(i == f)
+        self.assertTrue(j == k)
+        self.assertTrue(f == l)
 
     def test_init(self):
         with self.assertRaises(TypeError):
@@ -53,3 +57,7 @@ class FractionTest(unittest.TestCase):
             Fraction(1, 0)
         with self.assertRaises(ValueError):
             Fraction(-1, 0)
+
+    def test_mul(self):
+        self.assertEqual(Fraction(6), Fraction(2)*Fraction(3))
+        self.assertEqual(Fraction(2, 27), Fraction(1, 3)*Fraction(2, 9))
